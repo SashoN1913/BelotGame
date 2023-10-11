bc3bf3f509e8ce0aacaeebf2cbc8d0038fae4187
Aleksander <Aleksander@DESKTOP-HSQDH4Q>
2023-10-12 00:22:57 +0300

    CardsTest tested

--- BelotGame/src/belot/CardsTest.java
+++ BelotGame/src/belot/CardsTest.java
@@ -2,5 +2,10 @@ package belot;
 
 public class CardsTest 
 {
-	
+	public static void main(String[] args)
+	{
+		Cards firstCard = new Cards("ace", "spades");
+		Cards secondCard = new Cards("truck", "hearts");
+		System.out.println(secondCard);
+	}
 }
