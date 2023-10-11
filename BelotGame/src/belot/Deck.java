3411b69f008d988323be0c6afc07c46d15b39a26
Aleksander <Aleksander@DESKTOP-HSQDH4Q>
2023-10-12 00:39:38 +0300

    Deck constructors created

--- BelotGame/src/belot/Deck.java
+++ BelotGame/src/belot/Deck.java
@@ -1,5 +1,20 @@
 package belot;
 
-public class Deck {
+import java.util.ArrayList;
 
+
+public class Deck 
+{
+	private ArrayList<Cards> deck = new ArrayList<>();
+
+	public Deck(ArrayList<Cards> deck) {
+		super();
+		this.deck = deck;
+	}
+	
+	public Deck()
+	{
+		ArrayList<String> suits = Cards.getValidSuits();
+		ArrayList<String> faceName = Cards.getValidCards();
+	}
 }
