b59b5d53abaae73280bfeaad2eb9bbbc38e3cfae
Aleksander <Aleksander@DESKTOP-HSQDH4Q>
2023-10-11 23:57:10 +0300

    Constructor get and set methods added

--- BelotGame/src/belot/Cards.java
+++ BelotGame/src/belot/Cards.java
@@ -3,5 +3,27 @@ package belot;
 public class Cards 
 {
 	private String faceName, suit;
+
+	public String getFaceName() {
+		return faceName;
+	}
+
+	public void setFaceName(String faceName) {
+		this.faceName = faceName;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public Cards(String faceName, String suit) {
+		super();
+		this.faceName = faceName;
+		this.suit = suit;
+	}
 	
 }
