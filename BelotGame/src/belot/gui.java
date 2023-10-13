a54150e8d010ec4793e683b820141153f489e313
Aleksander <Aleksander@DESKTOP-HSQDH4Q>
2023-10-14 01:24:29 +0300

    Deals 3 more cards if player is not pass

--- BelotGame/src/belot/gui.java
+++ BelotGame/src/belot/gui.java
@@ -4,6 +4,11 @@ import java.awt.EventQueue;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,6 +17,9 @@ import javax.swing.text.html.ImageView;
 import javax.swing.JButton;
 import javax.swing.JToolBar;
 import java.awt.Choice;
+import java.awt.Label;
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
 
 public class gui {
 
@@ -50,17 +58,21 @@ public class gui {
 		deck = new Deck();
 		frame = new JFrame();
 		frame.setBounds(100, 100, 537, 386);
+		//frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		
+		ArrayList<String> newDeckOfCards = deck.deckWithNames();
+		Collections.shuffle(newDeckOfCards);
 		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(-1, 11, 50, 100);
 		Image img = new ImageIcon(this.getClass().getResource("/backOfCardss.jpg")).getImage();
+		frame.getContentPane().setLayout(null);
 		lblNewLabel.setIcon(new ImageIcon(img));
-		lblNewLabel.setBounds(-1, 0, 512, 111);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel firstCard = new JLabel("");
-		firstCard.setBounds(35, 216, 72, 120);
+		firstCard.setBounds(61, 216, 72, 120);
 		frame.getContentPane().add(firstCard);
 		
 		JButton btnDealCards = new JButton("Раздаване");
@@ -83,40 +95,84 @@ public class gui {
 		fifthCard.setBounds(421, 216, 78, 120);
 		frame.getContentPane().add(fifthCard);
 		
+		JLabel sixthCard = new JLabel("");
+		sixthCard.setBounds(61, 105, 78, 105);
+		frame.getContentPane().add(sixthCard);
 		
+		JLabel seventhCard = new JLabel("");
+		seventhCard.setBounds(163, 105, 78, 107);
+		frame.getContentPane().add(seventhCard);
 		
+		JLabel eigthCard = new JLabel("");
+		eigthCard.setBounds(267, 105, 72, 100);
+		frame.getContentPane().add(eigthCard);
+		
+		Choice choice = new Choice();
+		choice.setBounds(163, 79, 118, 20);
+		frame.getContentPane().add(choice);
+		choice.add("Пас");
+		choice.add("Спатия");
+		choice.add("Каро");
+		choice.add("Пика");
+		choice.add("Без коз");
+		choice.add("Всичко коз");
 		btnDealCards.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) 
 			{	
-				String [] card = deck.deckWithNames();
-				System.out.println(card[0]);
-				card = deck.shuffleDeck(card);
+				ArrayList<String> card = newDeckOfCards;
+				//System.out.println(card[0]);
 				for(int i = 0; i < 5; i++)
 				{
-					System.out.println(card[i]);
+					//System.out.println(card.get(i));
 				}
-				System.out.println(card[0]);
+				//System.out.println("Тест" + card.get(2));
 				//System.out.println(card[0]);
 				//Image img = new ImageIcon(this.getClass().getResource("/" + card[0])).getImage();
-				firstCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card[0])));
-				secondCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card[1])));
-				thirdCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card[2])));
-				fourthCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card[3])));
-				fifthCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card[4])));
-				Choice choice = new Choice();
-				choice.setBounds(181, 105, 118, 120);
-				frame.getContentPane().add(choice);
-				choice.add("Пас");
-				choice.add("Спатия");
-				choice.add("Каро");
-				choice.add("Пика");
-				choice.add("Без коз");
-				choice.add("Всичко коз");
+				firstCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card.get(0))));
+				secondCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card.get(1))));
+				thirdCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card.get(2))));
+				fourthCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card.get(3))));
+				fifthCard.setIcon(new ImageIcon(this.getClass().getResource("/" + card.get(4))));
+				
+				//System.out.println("peta kart" + card.get(5));
+				System.out.println(card);
 			}
 			
+			
 		});
+		for(int i = 0; i < 4; i++)
+		{
+			newDeckOfCards.remove(i);
+		}
+		System.out.println(newDeckOfCards.get(0));
+		
+		newDeckOfCards.trimToSize();
+		choice.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				//System.out.println(newDeckOfCards);
+				for(int i = 0; i < 5; i++)
+				{
+					newDeckOfCards.remove(0);
+					newDeckOfCards.trimToSize();
+				}
+				//newDeckOfCards.trimToSize();
+				System.out.println(newDeckOfCards);
+				if(e.getSource() == choice)
+				{
+					 if(!choice.getSelectedItem().equals("Пас")) 
+					  { 
+						 sixthCard.setIcon(new ImageIcon(this.getClass().getResource("/" + newDeckOfCards.get(0))));
+						 seventhCard.setIcon(new ImageIcon(this.getClass().getResource("/" + newDeckOfCards.get(1))));
+						 eigthCard.setIcon(new ImageIcon(this.getClass().getResource("/" + newDeckOfCards.get(2))));
+					  }
+				}
+				
+			}});
+		 
 	
 
 	}
